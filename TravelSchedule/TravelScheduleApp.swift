e12286f9d039761a91d739cd600ad81b36989ee9
Andrei Boyarko <andreyboyarko@gmail.com>
2025-11-04 22:02:47 +0100

    Initial commit: project setup and API service

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Andrei  Boyarko on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
